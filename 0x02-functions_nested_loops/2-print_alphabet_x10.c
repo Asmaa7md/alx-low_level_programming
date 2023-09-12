18b1193e1ae3a4827f65bae9524701a1646323e5
Asmaa7md <asmaa7md98@gmail.com>
2023-09-12 16:04:42 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,9 @@
  * main - check the code.
  *
  * Describtion: printing alphabet 10 times
+ *
+ * print_alphabet_x10: function printing alphabet 10 times
+ *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)

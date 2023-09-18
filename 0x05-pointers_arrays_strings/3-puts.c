75523080c4e21feec60ba4d28584dc5cf198590e
Asmaa7md <asmaa7md98@gmail.com>
2023-09-18 15:29:09 -0700

    5

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @str: pointer to a string
+ * Return: no return
+ * _puts - prints a string followed with a new line
+ */
+void _puts(char *str)
+{
+	i = 0
+
+	while (str[i])
+		i++;
+	_putchar(str);
+	putchar(\n);
+}

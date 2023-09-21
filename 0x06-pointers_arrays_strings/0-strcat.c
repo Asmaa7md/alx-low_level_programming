ceb4ad6c40b71c556efadf69ef721bda46e81836
Asmaa7md <asmaa7md98@gmail.com>
2023-09-21 14:00:09 -0700

    6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,10 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	x = 0;
-	while (dest[x] = !'\0')
+	while (dest[x] != '\0')
 		x++;
 	y = 0;
-	while (src[y] = !'\0')
+	while (src[y] != '\0')
 		dest[x] = srt[x];
 		x++;
 		y++;

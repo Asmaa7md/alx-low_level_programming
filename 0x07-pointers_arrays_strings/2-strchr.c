811f7892fc4bc125aa1007d9a3507aa303388f7b
Asmaa7md <asmaa7md98@gmail.com>
2023-09-25 13:07:20 -0700

    7

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * _strchr - locates a character in a string.
+ * @s: pointer string
+ * @c: character
+ * Return: *s or 0
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s == c)
+	{
+		return (s);
+		s++;
+	}
+		return (0);
+}

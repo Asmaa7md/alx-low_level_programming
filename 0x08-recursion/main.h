319dc54146579cb36393f11ddbec0b1853e6d9df
Asmaa7md <asmaa7md98@gmail.com>
2023-09-28 16:51:03 -0700

    8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,7 +12,7 @@ int square(int n, int x);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int check(char *s, int start, int end, int pair)
-int last_index(char *s)
+int check(char *s, int start, int end, int pair);
+int last_index(char *s);
 
 #endif

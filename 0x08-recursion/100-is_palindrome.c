84d054e783579b34f4b95a9cbb3f8b2808ecf052
Asmaa7md <asmaa7md98@gmail.com>
2023-09-28 15:47:54 -0700

    8

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - defines if the string is palindrome
+ * @s: pointer to a string
+ * Return: 0 or 1
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1);
+}

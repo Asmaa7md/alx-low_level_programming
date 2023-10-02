c305ce361380e0b3ed84f015794b0c1d6646b691
Asmaa7md <asmaa7md98@gmail.com>
2023-10-02 10:09:22 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number
+ * @argv: pointer to characters array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if(x < argv)
+	{
+		printf("%s\n", x);
+		x++;
+	}
+	return (0);
+}
+

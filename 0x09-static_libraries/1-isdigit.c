2ced789acb341d02bfe21aa3d03978cd09706eb7
Asmaa7md <asmaa7md98@gmail.com>
2023-10-02 07:02:18 -0700

    9

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check for a digit
+ * Return: 0 or 1.
+ * @c : character to check
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}

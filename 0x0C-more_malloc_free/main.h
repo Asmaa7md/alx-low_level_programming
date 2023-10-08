2f87a7c63691fd9094a7c0242e2377b9733e5e97
Asmaa7md <asmaa7md98@gmail.com>
2023-10-08 14:28:13 -0700

    c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,8 +5,8 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *s, char x, unsigned int n);
-
-
+int _putchar(char c);
+int *array_range(int min, int max);
 
 
 
